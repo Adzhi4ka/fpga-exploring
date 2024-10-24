930feefedefca4b01e9d762dfebd64ead505a18e
Adzhi4ka <y88005553535@yandex.ru>
2024-10-24 15:17:51 +0300

    fix

--- serializer/rtl/serializer.sv
+++ serializer/rtl/serializer.sv
@@ -1,12 +1,12 @@
-parameter DATA_W = 16;
-parameter DATA_MOD_W = 4;
+module serializer #(
+  parameter DATA_W     = 16,
+  parameter DATA_MOD_W = 4
+)(
+  input  logic                     clk_i,
+  input  logic                     srst_i,
 
-module serializer(
-  input  logic                      clk_i,
-  input  logic                      srst_i,
-
-  input  logic [DATA_W - 1:0]      data_i,
-  input  logic [DATA_MOD_W - 1:0]  data_mod_i,
+  input  logic [DATA_W-1:0]        data_i,
+  input  logic [DATA_MOD_W-1:0]    data_mod_i,
   input  logic                     data_val_i,
 
   output logic                     ser_data_o,
@@ -15,52 +15,75 @@ module serializer(
   output logic                     busy_o
 );
 
-logic [DATA_W - 1:0]     data_i_buff;
-logic [DATA_MOD_W - 1:0] data_mod_i_buff;
-logic [DATA_MOD_W:0] counter;
+localparam COUNTER_W = DATA_MOD_W + 1;
 
+logic [DATA_W-1:0]     data_i_buff;
+logic [DATA_MOD_W-1:0] data_mod_i_buff;
+logic [COUNTER_W-1:0]  counter;
+
+// data_i_buff, data_mod_i_buff
 always_ff @( posedge clk_i )
   begin
     if ( srst_i )
       begin
-        data_i_buff     <= 0;
-        data_mod_i_buff <= 0;
-        ser_data_o      <= 0;
-        ser_data_val_o  <= 0;
-        busy_o          <= 0;
+        data_i_buff     <= '0;
+        data_mod_i_buff <= '0;
       end
-    else 
-      begin
-        if ( data_val_i && !busy_o )
-          begin
-            if ( data_mod_i >= 1 && data_mod_i <= 2 )
-              busy_o <= 0;
-            else
-              begin
-                data_i_buff     <= data_i;
-                data_mod_i_buff <= data_mod_i == 0 ? 3'b0 : data_mod_i - 1'b1;
-                counter         <= 0;
-                busy_o          <= 1;
-              end
-          end
+    else
+      if ( ( data_val_i ) && ( data_mod_i != 'b1 ) && ( data_mod_i != 'b10 ) )
+        begin
+          data_i_buff     <= data_i;
+          data_mod_i_buff <= !data_mod_i ? (DATA_MOD_W)'(0) : data_mod_i - 1'b1;
+        end
+  end
 
-        if (busy_o)
-          begin
-            if (( data_mod_i_buff == 0 && counter == ( DATA_W ) ) || 
-                ( counter > data_mod_i_buff && data_mod_i_buff != 0 )
-            )
-              begin
-                busy_o         <= 0;
-                ser_data_val_o <= 0;
-              end
-            else
-              begin
-                ser_data_o      <= data_i_buff[DATA_W - 1 - counter];
-                ser_data_val_o  <= 1;
-                counter         <= counter + 1'b1;
-              end
-          end
-      end
+// counter
+always_ff @( posedge clk_i )
+  begin
+    if ( srst_i )
+      counter <= '0;
+    else
+      if ( ( data_val_i ) && ( data_mod_i != 'b1 ) && ( data_mod_i != 'b10 ) )
+        counter <= '0;
+      else
+        if ( busy_o )
+          counter <= counter + 1'b1;
+  end
+
+// busy_o
+always_ff @( posedge clk_i )
+  begin
+    if ( srst_i )
+        busy_o <= 1'b0;
+    else
+      if ( data_val_i && data_mod_i != 'b1 && data_mod_i != 'b10 )
+        busy_o <= 1'b1;
+      else if ( ( ( !data_mod_i_buff ) && ( counter == ( DATA_W )    ) ) || 
+                ( ( counter > data_mod_i_buff ) && ( data_mod_i_buff ) ) )
+        busy_o <= 1'b0;
+  end
+
+// ser_data_val_o
+always_ff @( posedge clk_i )
+  begin
+    if ( srst_i )
+        ser_data_val_o <= 1'b0;
+    else
+      if ( busy_o )
+        if ( ( ( !data_mod_i_buff ) && counter == ( DATA_W )     ) || 
+             ( counter > data_mod_i_buff &&  ( data_mod_i_buff ) ) )
+          ser_data_val_o <= 1'b0;
+        else
+          ser_data_val_o <= 1'b1;
+  end
+
+// ser_data_o
+always_ff @( posedge clk_i )
+  begin
+    if ( srst_i )
+      ser_data_o <= 1'b0;
+    else
+      ser_data_o <= data_i_buff[(DATA_W - 1 - counter) & {DATA_MOD_W{1'b1}}];
   end
 
 endmodule
--- serializer/tb/serializer_tb.sv
+++ serializer/tb/serializer_tb.sv
@@ -1,83 +1,192 @@
 module serializer_tb;
 
-bit clk;
+parameter DATA_W     = 16;
+parameter DATA_MOD_W = 4;
+
+typedef struct {
+  logic [DATA_W-1:0]     data;
+  logic [DATA_MOD_W-1:0] data_mod;
+} test_arg;
+
+bit                    clk;
+bit                    srst;
+bit                    rst_done;
+
+logic [DATA_W-1:0]     data;
+logic [DATA_MOD_W-1:0] data_mod;
+bit                    is_data_val;
+
+bit                    ser_bit_data;
+bit                    is_ser_data_val;
+
+bit                    is_serializer_busy;
+
 initial
   forever
     #5 clk = !clk;
 
-bit sync_reset;
-logic [15:0] input_data;
-logic [4:0] input_data_mod;
-bit is_data_valid;
+default clocking cb
+  @ (posedge clk);
+endclocking
 
-bit serialized_bit_data;
-bit is_valid_serialized_bit_data;
+serializer #(
+  .DATA_W         ( DATA_W             ),
+  .DATA_MOD_W     ( DATA_MOD_W         )
+) serializer_test (
+  .clk_i          ( clk                ),
+  .srst_i         ( srst               ),
 
-bit is_serializer_busy;
+  .data_i         ( data               ),
+  .data_mod_i     ( data_mod           ),
+  .data_val_i     ( is_data_val        ),
 
-serializer serializer_test (
-  .clk_i          ( clk                          ),
-  .srst_i         ( sync_reset                   ),
+  .ser_data_o     ( ser_bit_data       ),
+  .ser_data_val_o ( is_ser_data_val    ),
 
-  .data_i         ( input_data                   ),
-  .data_mod_i     ( input_data_mod               ),
-  .data_val_i     ( is_data_valid                ),
-
-  .ser_data_o     ( serialized_bit_data          ),
-  .ser_data_val_o ( is_valid_serialized_bit_data ),
-
-  .busy_o         ( is_serializer_busy           )
+  .busy_o         ( is_serializer_busy )
 );
 
-int test_file;
-int file_data;
-int file_data_mod;
+mailbox #( test_arg ) generated_data = new();
+mailbox #( test_arg ) sended_data    = new();
+mailbox #( test_arg ) readed_data    = new();
+
+task gen_data( input   int           cnt,
+               mailbox #( test_arg ) data );
+  test_arg data_to_send;
+
+  for ( int i = 0; i < cnt; ++i)
+    begin
+      data_to_send.data     = $urandom_range(2**DATA_W - 1, 0);
+      data_to_send.data_mod = $urandom_range(2**DATA_MOD_W - 1, 0);
+      data.put( data_to_send );
+    end
+
+endtask
+
+task serializer_wr( mailbox #( test_arg ) generated_data,
+                    mailbox #( test_arg ) sended_data    );
+  test_arg arg;
+
+  while ( generated_data.num() )
+    begin
+      ##1
+      if ( !is_serializer_busy )
+        begin
+          generated_data.get(arg);
+          sended_data.put(arg);
+
+          data        <= arg.data;
+          data_mod    <= arg.data_mod;
+          is_data_val <= 1'b1;
+
+          ##1
+          is_data_val <= 1'b0;
+        end
+    end
+
+  wait( !is_serializer_busy );
+    
+  
+  ##1
+  srst <= 1'b1;
+  ##1
+  srst <= 1'b0;
+endtask
+
+task serializer_r( mailbox #( test_arg ) readed_data );
+  test_arg readed;
+
+  forever
+    begin
+      if ( srst )
+        begin
+          readed_data.put(readed);
+          return;
+        end
+
+      ##1
+      if ( is_serializer_busy && is_ser_data_val )
+        begin
+          readed.data[readed.data_mod] <= ser_bit_data;
+          readed.data_mod              <= readed.data_mod + 1'b1;
+        end
+
+      if ( is_data_val )
+        begin
+          readed_data.put(readed);
+          readed.data       <= '0;
+          readed.data_mod   <= '0;
+        end
+    end  
+endtask
+
+task compare_data (mailbox #( test_arg ) sended_data,
+                   mailbox #( test_arg ) readed_data);
+  test_arg sended;
+  test_arg readed;
+
+  readed_data.get(readed); // 0-ой элемент - мусор
+
+  if ( sended_data.num() != readed_data.num() )
+    begin
+      $display( "Size of ref data: %d", sended_data.num() );
+      $display( "And sized of dut data: %d", readed_data.num() );
+      $display( "Do not match" );
+      $stop();
+    end
+  
+  for (int i = 0; i < sended_data.num(); ++i)
+    begin
+      sended_data.get(sended);
+      readed_data.get(readed);
+
+      if ((sended.data_mod == 1 || sended.data_mod == 2) && (readed.data_mod == 0))
+        continue;
+    
+      if (sended.data_mod != readed.data_mod)
+        begin
+          $display("ERROR! Data mod don`t match, %d", i);
+          $display("Reference data mod: %d", sended.data_mod);
+          $display("Readed data mod: %d", readed.data_mod);
+          $stop();
+        end
+      
+      for (int j = 0; j < sended.data_mod; ++j)
+        begin
+          if (sended.data[2**DATA_W - 1 - j] != readed.data[j])
+            begin
+              $display("ERROR! Data don`t match. (Readed data is reversed)");
+              $display("Reference data: %d", sended.data);
+              $display("Readed data: %d", readed.data);
+              $stop();
+            end
+        end
+    end
+endtask
 
 initial
   begin
-    test_file = $fopen("/home/adzhi4ka/fpga-exploring/serializer/tb/test.txt", "r");
+    srst     <= 1'b0;
+    ##1;
+    srst     <= 1'b1;
+    ##1;
+    srst     <= 1'b0;
+    rst_done <= 1'b1;
+  end
 
-    if( !test_file )
-      begin
-        $display("File test.txt was NOT found!");
-        $stop();
-      end
+initial 
+  begin
+    gen_data( 100, generated_data );
 
-    @( posedge clk );
-    sync_reset <= 1'b1;
-    @( posedge clk );
-    sync_reset <= 1'b0;
+    wait( rst_done );
 
-    while( !$feof(test_file) )
-      begin
-        $fscanf(test_file, "%d", file_data);
-        $fscanf(test_file, "%d", file_data_mod);
+    fork
+      serializer_wr(generated_data, sended_data);
+      serializer_r(readed_data);
+    join
 
-        $display("%b, %b \n", input_data, input_data_mod);
-
-        @( posedge clk );
-        input_data     <= file_data;
-        input_data_mod <= file_data_mod;
-        is_data_valid  <= 1;
-
-        @( posedge clk );
-        is_data_valid  <= 0;
-
-        @( posedge clk );
-        for (int i = 0; i < file_data_mod; i++)
-          begin
-            @( posedge clk );
-            if (( is_serializer_busy )                         &&
-                ( !is_valid_serialized_bit_data )              && 
-                ( serialized_bit_data != input_data[15 - i] ))
-              begin
-                $display("Test failed\n");
-                $stop();
-              end
-          end      
-      end
-
-    $display("Test passed\n");  
+    compare_data(sended_data, readed_data);
+    $display("TEST PASSED!");
     $stop();
   end
 
