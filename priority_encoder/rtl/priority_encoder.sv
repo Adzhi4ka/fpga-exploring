af4cd1df2a4d0c3e8c4c53b71e7d0f6c9870a9a0
Adzhi4ka <y88005553535@yandex.ru>
2024-11-06 00:44:53 +0300

    final realization

--- priority_encoder/rtl/priority_encoder.sv
+++ priority_encoder/rtl/priority_encoder.sv
@@ -12,7 +12,22 @@ module priority_encoder #(
   output logic             data_val_o
 );
 
-logic [DATA_W-1:0] reverse_data_i;
+function logic [DATA_W-1:0] ReverseBits(logic [DATA_W-1:0] data);
+  logic [DATA_W-1:0] reversed_data;
+
+  for (integer i = 0; i < DATA_W; i++) 
+    reversed_data[i] = data[DATA_W-1-i];
+
+  return reversed_data;
+endfunction
+
+logic [DATA_W-1:0] reversed_data;
+logic [DATA_W-1:0] reversed_data_left;
+
+assign reversed_data      = ReverseBits(data_i);
+assign reversed_data_left = reversed_data & (-reversed_data);
+assign data_left_o        = ReverseBits(reversed_data_left);
+assign data_right_o       = data_i & (-data_i);
 
 // data_val_o
 always_ff @( posedge clk_i )
@@ -20,32 +35,7 @@ always_ff @( posedge clk_i )
     if ( srst_i ) 
       data_val_o <= '0;
     else
-      if ( data_val_i )
-        data_val_o <= '1;
-      else
-        data_val_o <= '0;
+      data_val_o <= data_val_i;
   end
 
-// data_right_o
-always_comb
-  begin
-    data_right_o <= data_i & (~data_i + 1'b1);
-  end
-
-// reverse_data_i
-always_comb 
-  begin
-    for (int i = DATA_W-1; i >= 0; --i)
-      reverse_data_i[DATA_W - 1 - i] = data_i[i];
-
-    reverse_data_i = reverse_data_i & (~reverse_data_i + 1'b1);
-  end
-
-// data_left_o
-always_comb 
-  begin
-    for (int i = DATA_W-1; i >= 0; --i)
-      data_left_o[DATA_W - 1 - i] = reverse_data_i[i];
-  end
-    
 endmodule
